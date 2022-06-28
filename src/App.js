befa381dfed4d071974ba54dace2f40a8994c272
dastanmurataliev <dastan_murataliev@inbox.ru>
2022-06-28 15:52:11 +0600

    DZ-5_Dastan-13-01

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import InputPage from './pages/inputPage/InputPage';
+// import MainPageFunc from './pages/mainPagFunc/MainPageFunc';
+// import MainPageClass from './pages/mainPageClass/MainPageClass';
+
+function App() {
+  return (
+    <div>  
+       <InputPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/usersList/UsersList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UsersList = ({users})  => (
+  <ul>
+    {users.map((user,key) => <li key={key}>{user}</li>)}
+  </ul>
+);
+  
+
+export default UsersList;
